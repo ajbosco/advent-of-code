9bfa85e85b70ebc29adb9cd65c7e8cde26227080
Adam Boscarino <ajboscarino@gmail.com>
2018-12-01 13:19:06 -0500

    day 1 solution

---
+++ 2018/day-01/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+
+	"github.com/sirupsen/logrus"
+)
+
+const inputFile = "input"
+
+func main() {
+	c, err := newCalibrator(inputFile)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	c.calibrate()
+}
+
+type calibrator struct {
+	rawData          []string
+	processedData    []int64
+	frequencyHistory map[int64]int
+}
+
+func newCalibrator(path string) (*calibrator, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var rawData []string
+	var processedData []int64
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		rawData = append(rawData, scanner.Text())
+		val, err := strconv.ParseInt(scanner.Text(), 10, 64)
+		if err != nil {
+			logrus.Error(err)
+		}
+		processedData = append(processedData, val)
+	}
+	return &calibrator{rawData: rawData, processedData: processedData}, scanner.Err()
+}
+
+func (c *calibrator) calibrate() {
+	var frequency int64
+	c.frequencyHistory = make(map[int64]int)
+
+	for c.frequencyHistory[frequency] != 2 {
+		for _, change := range c.processedData {
+			currentFrequency := frequency
+			frequency += change
+			c.frequencyHistory[frequency]++
+			logrus.Infof("Current frequency %v, change of %v; resulting frequency %v", currentFrequency, change, frequency)
+			if c.frequencyHistory[frequency] == 2 {
+				break
+			}
+		}
+	}
+	logrus.Infof("First duplicate frequency is %v", frequency)
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Advent of Code
+
+Solutions (and attempts) for [Advent of Code](https://adventofcode.com)
